04e64b7047a15fa553a2f1bb6a303d12f023db96
Ashwin Mishra <amishra01@student.ysu.edu>
2020-11-22 18:36:59 -0500

    minor fixes

--- src/lag.cpp
+++ src/lag.cpp
@@ -8,15 +8,20 @@
 
 int main(int argc, char ** args) {
   if (argc < 2) {
-    cout << "Please enter two files: " << endl;
-    cout << "1. Input specifications (See files/input.specs)" << endl;
-    cout << "2. An output file for Lexical Analyzer (eg: output.cpp or lexicalAnalyzer.cpp)" << endl;
+    cout << "Please enter two files: \n";
+    cout << "1. Input specifications (See files/input.specs) \n";
+    cout << "2. An output file for Lexical Analyzer (eg: output.cpp or lexicalAnalyzer.cpp) \n";
   }
-  cout << "LAG (Lexical Analzer Generator) - Project for Compiler Design by Ashwin Mishra" << endl;
-  cout << "------------------------------------------------------------------------------" << endl;
+
+  cout << "LAG (Lexical Analzer Generator) - Project for Compiler Design by Ashwin Mishra \n";
+  cout << "------------------------------------------------------------------------------ \n";
+
   string input = args[1];
   string outfile = args[2];
+
   Parser p(input);
+
   p.convertToC(outfile);
+  
   return 0;
 }
